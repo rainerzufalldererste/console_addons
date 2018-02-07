3a765b8c394cb19b3523dcc1e0ec80b9f52b0d05
Christoph Stiller <c.stiller@live.de>
2018-02-07 23:21:08 +1000

    adding .catch(); doesn't seem to help much...

--- cdf/Program.cs
+++ cdf/Program.cs
@@ -14,20 +14,22 @@ namespace cdf
     static void Main(string[] args)
     {
       IEnumerable<string> dirs = null;
-      
+
       if (args.Length == 1)
       {
         args[0] = args[0].Replace("/", "?").Replace("\\", "?");
-        dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, args[0], SearchOption.AllDirectories);
+        dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, args[0], SearchOption.AllDirectories).Catch(typeof(UnauthorizedAccessException)).ToArray();
       }
       else if (args.Length == 2 && args[0] == "-t")
       {
         args[1] = args[1].Replace("/", "?").Replace("\\", "?");
-        dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, args[1], SearchOption.TopDirectoryOnly);
+        dirs = Directory.EnumerateDirectories(Environment.CurrentDirectory, args[1], SearchOption.TopDirectoryOnly).Catch(typeof(UnauthorizedAccessException)).ToArray();
       }
       else
       {
         Console.WriteLine("Invalid Parameter.\n\ncdf [-t(op directories only)] <directory-search-pattern>");
+
+        return;
       }
 
       if (dirs.Count() == 0)
@@ -42,21 +44,21 @@ namespace cdf
           dir = dir.Substring(Environment.CurrentDirectory.Length + 1);
 
         Console.WriteLine(dir);
-        
+
         System.Windows.Forms.Clipboard.SetText(dir);
         Console.WriteLine("\nCopied directory name to Clipboard.");
       }
       else
       {
         bool first = true;
-        
+
         foreach (string d in dirs.OrderBy((s) => (from c in s where c == '\\' || c == '/' select 0).Count()))
         {
           string dir = d;
-          
+
           if (dir.StartsWith(Environment.CurrentDirectory))
             dir = dir.Substring(Environment.CurrentDirectory.Length + 1);
-          
+
           if (first)
           {
             try
@@ -64,17 +66,48 @@ namespace cdf
               System.Windows.Forms.Clipboard.SetText(dir);
             }
             catch { }
-            
+
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"{dir} (Copied)");
             Console.ResetColor();
-            
+
             first = false;
           }
           else
           {
             Console.WriteLine(dir);
           }
+        }
+      }
+    }
+  }
+
+  // See: http://qaru.site/questions/127277/c-handle-systemunauthorizedaccessexception-in-linq
+  static class ExceptionExtensions
+  {
+    public static IEnumerable<TIn> Catch<TIn>(this IEnumerable<TIn> source, Type exceptionType)
+    {
+      using (var enumerator = source.GetEnumerator())
+      {
+        while (true)
+        {
+          bool ok = false;
+
+          try
+          {
+            ok = enumerator.MoveNext();
+          }
+          catch (Exception e)
+          {
+            if (e.GetType() != exceptionType)
+              throw;
+            continue;
+          }
+
+          if (!ok)
+            yield break;
+
+          yield return enumerator.Current;
         }
       }
     }
