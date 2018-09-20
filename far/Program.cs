9722f8129b62dc122942b4772fbb1298e27ad443
Christoph Stiller <cstiller@euclideon.com>
2018-09-20 17:15:19 +1000

    Adding far.exe for opening random files

---
+++ far/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace far
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        Console.WriteLine("Usage:\nfar.exe [IGNORED] <URL>");
+      }
+      else
+      {
+        try
+        {
+          System.Diagnostics.Process.Start(args.Last().Trim());
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine($"Execution Failed. ({e.Message})");
+        }
+      }
+    }
+  }
+}
